69923cf2f634f13bd1b9f63ed101a04860473981
zhangwenjun <zhangwenjun32317128@icloud.com>
2022-09-07 00:34:03 +0800

    init

---
+++ leetcode/leetcode/src/main/java/leetcode/editor/cn/PTXy4P.java
@@ -0,0 +1,27 @@
+
+package leetcode.editor.cn;
+import org.springframework.util.StopWatch;
+
+public class PTXy4P{
+  public static void main(String[] args) {
+        Solution solution = new PTXy4P().new Solution();
+        StopWatch watch = new StopWatch("总共耗时");
+        watch.start("采集果实");
+        // 
+        watch.stop();
+        System.out.println(watch.prettyPrint());
+  }
+  /**
+  * 采集果实
+  * PTXy4P
+  */
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int getMinimumTime(int[] time, int[][] fruits, int limit) {
+
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
